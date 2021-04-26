4c5579db8402dd00e5c82d477fcce29ce8574962
Diogo Alexandre <diogo.alexandre023@gmail.com>
2021-04-26 20:46:37 -0300

    add profile test config file and add seeds

---
+++ src/main/java/com/example/demospring/config/TestConfig.java
@@ -0,0 +1,29 @@
+package com.example.demospring.config;
+
+import com.example.demospring.entities.User;
+import com.example.demospring.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+@Profile(value = "test")
+public class TestConfig implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        List<User> users = new ArrayList<>();
+
+        users.add(new User(null, "Diogo", "diogo@mail.com", "+5500999999999", "12345"));
+        users.add(new User(null, "John", "john@mail.com", "+5500999999999", "12345"));
+
+        userRepository.saveAll(users);
+    }
+}
