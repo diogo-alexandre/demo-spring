fef5d0a15b666d7cebe31dd1b2e833c9c14fcbfd
Diogo Alexandre <diogo.alexandre023@gmail.com>
2021-04-26 19:07:57 -0300

    update user entity to javax.persistence

--- src/main/java/com/example/demospring/entities/User.java
+++ src/main/java/com/example/demospring/entities/User.java
@@ -1,10 +1,17 @@
 package com.example.demospring.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
 public class User implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     public Long id;
     public String name;
     public String email;
