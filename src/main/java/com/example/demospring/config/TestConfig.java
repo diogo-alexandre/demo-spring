0cdb961435d9fbd42d3bca14f300931086b30862
Diogo Alexandre <diogo.alexandre023@gmail.com>
2021-04-27 04:54:53 -0300

    update test config and add order seed

--- src/main/java/com/example/demospring/config/TestConfig.java
+++ src/main/java/com/example/demospring/config/TestConfig.java
@@ -1,32 +1,40 @@
 package com.example.demospring.config;
 
+import com.example.demospring.entities.Order;
 import com.example.demospring.entities.User;
+import com.example.demospring.repositories.OrderRepository;
 import com.example.demospring.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.time.Instant;
+import java.util.Arrays;
 
 @Configuration
 @Profile(value = "test")
 public class TestConfig implements CommandLineRunner {
     private final UserRepository userRepository;
+    private final OrderRepository orderRepository;
 
     @Autowired
-    public TestConfig(UserRepository userRepository) {
+    public TestConfig(UserRepository userRepository, OrderRepository orderRepository) {
         this.userRepository = userRepository;
+        this.orderRepository = orderRepository;
     }
 
     @Override
     public void run(String... args) {
-        List<User> users = new ArrayList<>();
 
-        users.add(new User(null, "Diogo", "diogo@mail.com", "+5500999999999", "12345"));
-        users.add(new User(null, "John", "john@mail.com", "+5500999999999", "12345"));
+        User u1 = new User(null, "Diogo", "diogo@mail.com", "+5500999999999", "12345");
+        User u2 = new User(null, "John", "john@mail.com", "+5500999999999", "12345");
 
-        userRepository.saveAll(users);
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:03Z"), u1);
+        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:01Z"), u2);
+        Order o3 = new Order(null, Instant.parse("2019-07-21T15:21:22Z"), u1);
+
+        userRepository.saveAll(Arrays.asList(u1, u2));
+        orderRepository.saveAll(Arrays.asList(o1, o2, o3));
     }
 }
