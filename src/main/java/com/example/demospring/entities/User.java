4e0c893999aab36aa75328f547a1d9489e56988a
Diogo Alexandre <diogo.alexandre023@gmail.com>
2021-04-27 04:53:46 -0300

    update user entity

--- src/main/java/com/example/demospring/entities/User.java
+++ src/main/java/com/example/demospring/entities/User.java
@@ -1,10 +1,11 @@
 package com.example.demospring.entities;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -17,6 +18,10 @@ public class User implements Serializable {
     public String email;
     public String phone;
     public String password;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User () { }
 
@@ -66,6 +71,10 @@ public class User implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
     }
 
     @Override
