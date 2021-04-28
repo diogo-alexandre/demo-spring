18e36b6e951c675e7e536809dac74e01511b47d6
Diogo Alexandre <diogo.alexandre023@gmail.com>
2021-04-28 03:23:23 -0300

    add category repository

---
+++ src/main/java/com/example/demospring/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.example.demospring.repositories;
+
+import com.example.demospring.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
