f6b9200eed6735f9f1de10092e504e7e78acee20
Diogo Alexandre <diogo.alexandre023@gmail.com>
2021-04-28 03:25:21 -0300

    add product repository

---
+++ src/main/java/com/example/demospring/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.example.demospring.repositories;
+
+import com.example.demospring.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
